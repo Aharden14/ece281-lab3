77c302cea27a13e09f3f92dd935ae40e6d49ae04
aharden14 <194177783+Aharden14@users.noreply.github.com>
2025-03-30 16:04:30 -0600

    working code

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,28 +57,111 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
+        port (
+            i_clk, i_reset  : in    std_logic;
+            i_left, i_right : in    std_logic;
+            o_lights_L      : out   std_logic_vector(2 downto 0);
+            o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
 	end component thunderbird_fsm;
-
-	-- test I/O signals
 	
-	-- constants
+	--Inputs
+	signal w_L : std_logic := '0';
+	signal w_reset : std_logic := '0';
+	signal w_R : std_logic := '0';
+	signal w_clk : std_logic := '0';
+	
+	--Outputs
+	signal w_thunderbird : std_logic_vector(5 downto 0) := "000000";
+		
+	-- Clock period definitions
+	constant k_clk_period : time := 10 ns;
 	
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+	 uut: thunderbird_fsm port map (
+          i_left => w_L,
+          i_right => w_R,
+          i_reset => w_reset,
+          i_clk => w_clk,
+          o_lights_L(0) => w_thunderbird(5),
+          o_lights_L(1) => w_thunderbird(4),
+          o_lights_L(2) => w_thunderbird(3),
+          o_lights_R(0) => w_thunderbird(2),
+          o_lights_R(1) => w_thunderbird(1),
+          o_lights_R(2) => w_thunderbird(0)
+        );
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    clk_proc : process
+	begin
+		w_clk <= '0';
+        wait for k_clk_period/2;
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
+	sim_proc: process
+	begin
+		-- sequential timing		
+		w_reset <= '1';
+		wait for k_clk_period;
+		  assert w_thunderbird = "000000" report "bad reset" severity failure;
+		
+		w_reset <= '0';
+		wait for k_clk_period;
+		
+		w_R <= '1'; wait for k_clk_period;
+          assert w_thunderbird = "000100" report "should turn on first right light" severity failure;
+        wait for k_clk_period;
+          assert w_thunderbird = "000110" report "should turn on second right light" severity failure;
+        wait for k_clk_period;
+          assert w_thunderbird = "000111" report "should turn on third right light" severity failure;    
+          
+        w_reset <= '1';
+		wait for k_clk_period;
+		  assert w_thunderbird = "000000" report "bad reset" severity failure;
+		
+		w_R <= '0'; 
+		wait for k_clk_period*2;
+		
+		w_reset <= '0';
+		wait for k_clk_period;
+		
+          
+        w_L <= '1'; wait for k_clk_period;
+          assert w_thunderbird = "001000" report "should turn on first left light" severity failure;
+        wait for k_clk_period;
+          assert w_thunderbird = "011000" report "should turn on second left light" severity failure;
+        wait for k_clk_period;
+          assert w_thunderbird = "111000" report "should turn on third left light" severity failure;
+        
+        w_L <= '0'; 
+		wait for k_clk_period;
+		  
+        w_reset <= '1';
+		wait for k_clk_period;
+		  assert w_thunderbird = "000000" report "bad reset" severity failure;
+		
+		w_reset <= '0';
+		wait for k_clk_period;
+		
+		w_R <= '0'; 
+		wait for k_clk_period;
+		
+		
+		w_L <= '1'; w_R <= '1'; wait for k_clk_period;
+          assert w_thunderbird = "111111" report "should turn on all lights" severity failure;
+        wait for k_clk_period;
+          assert w_thunderbird = "000000" report "should turn off all lights" severity failure;
 	
+		wait;
+	end process;
 	-----------------------------------------------------	
 	
 end test_bench;
